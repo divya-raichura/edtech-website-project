1b3035500a8b463fe203613506e110800984cf54
divya-raichura <divyaraichura8@gmail.com>
2022-12-06 12:59:52 +0530

    login

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,5 +1,84 @@
-export default function Login() {
+import Card from "../ui/Card";
+import classes from "../courses/NewCourse.module.css";
+import { useState } from "react";
+import AuthService from "../../services/auth.service";
+import { useNavigate } from "react-router-dom";
+
+export default function Register() {
+  const [Loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const key = e.target.name;
+    const val = e.target.value;
+    setData({ ...data, [key]: val });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      ...data,
+    };
+
+    registerUser(formData);
+    navigate("/");
+  };
+
+  const registerUser = (formData) => {
+    setLoading(true);
+    AuthService.register(formData).then(setLoading(false));
+  };
+
+  if (Loading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
-    <h1>Login</h1>
-  )
+    <Card>
+      <form onSubmit={submitHandler} className={classes.form}>
+        <div className={classes.control}>
+          <input
+            type="text"
+            required
+            id="name"
+            name="name"
+            value={data.name}
+            onChange={handleChange}
+            placeholder="Name"
+          />
+        </div>
+        <div className={classes.control}>
+          <input
+            required
+            name="email"
+            value={data.email}
+            onChange={handleChange}
+            placeholder="Email"
+            type="Email"
+            id="email"
+          />
+        </div>
+
+        <div className={classes.control}>
+          <input
+            name="password"
+            value={data.password}
+            onChange={handleChange}
+            required
+            type="password"
+            placeholder="Password"
+            id="password"
+          />
+        </div>
+        <div className={classes.actions} style={{ textAlign: "center" }}>
+          <button>Register</button>
+        </div>
+      </form>
+    </Card>
+  );
 }
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+import Login from "../components/auth/Login";
+import AuthService from "../services/auth.service";
+
 export default function LoginPage() {
-  return <section style={{ textAlign: "center" }}>
-    <h1>Login</h1>
-  </section>;
+  const user = AuthService.getUser();
+  console.log(user);
+  if (!user) {
+    return (
+      <section style={{ textAlign: "center" }}>
+        <h1>Login</h1>
+        <Login></Login>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>Hi {user.user.name}</h1>
+      <Link to="/logout">Logout</Link>
+    </section>
+  );
 }
